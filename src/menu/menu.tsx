db838c449f5f47e444fa59628fb079f8b6a5a2fd
Afranio Alves <afranio777alves@gmail.com>
2022-08-07 22:07:12 +0100

    alterando menu

--- src/menu/menu.tsx
+++ src/menu/menu.tsx
@@ -1,30 +1,17 @@
-import { BrowserRouter, Route, Link, Switch} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styles from './Menu.module.scss'
-import Home from '../../src/pages/Produtos/index'
-import Fale from 'pages/Rotas/Fale/fale'
-import Funcionario from 'pages/Rotas/Funcionario/funcionario'
-import Venda from 'pages/Rotas/Venda/venda'
+
 export default function Menu() {
     return (
         <div className={styles.menu}>
-            <BrowserRouter>
-                <ul className={styles.menuitem}>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/Venda'>Catalogo</Link></li>
-                    <li><Link to='/Fale'>Fale conosco</Link></li>
-                    <li><Link to='/Funcionario'>Trabalhe conosco</Link></li>
-                </ul>
-                <main>
-                        <Switch>
-                            <Route exact path='/Home'>
-                                <Home/>
-                            </Route>
-                            <Route path='/Fale'><Fale/></Route>
-                            <Route path='/Funcionario'><Funcionario /></Route>
-                            <Route path='/Venda'><Venda /></Route>
-                        </Switch>
-                </main>
-                </BrowserRouter>
+
+            <ul className={styles.menuitem}>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/Venda'>Catalogo</Link></li>
+                <li><Link to='/Fale'>Fale conosco</Link></li>
+                <li><Link to='/Funcionario'>Trabalhe conosco</Link></li>
+            </ul>
+
         </div>
     )
 }
